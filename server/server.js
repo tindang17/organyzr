4a5ebf8af81fce4a9eb847cfe1f21fe17260ded0
Spencer Ang <boomerandzapper@gmail.com>
2017-06-19 17:28:50 +0000

    Setup signup form.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const ENV = process.env.ENV || "development";
 const body = require('body-parser');
 const cookies = require('cookie-parser');
+
+const knexConfig  = require("../knexfile");
+const knex        = require("knex")(knexConfig[ENV]);
+const knexLogger  = require('knex-logger');
 
 const webpack = {
   core: require('webpack'),
@@ -14,9 +19,25 @@ const compiler = webpack.core(webpack.config);
 
 const PORT = 8080;
 
+//Functions
+
+const add_user_local = require("./functions/add_user_local.js");
+const add_user_facebook = require("./functions/add_user_facebook.js");
 
 app.use(body.json());
 app.use(cookies());
+
+// Listen to POST requests to /users.
+app.post('/signup', function(req, res) {
+  // Get sent data.
+  console.log('req', req)
+  let user = req.body;
+  // Do a MySQL query.
+  console.log(user)
+  add_user_local(knex, user, res)
+
+
+});
 
 
 app.use(webpack.middleware(compiler, {
