700c357a53011e69abaf96728dde01fc7fb0f1f5
grantran <grant.tran@gmail.com>
2017-06-19 17:41:26 +0000

    multiple game cards, app js rewrite style

--- client/src/components/Games.jsx
+++ client/src/components/Games.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Button, Card, Image } from 'semantic-ui-react'
+import { Button, Card, Image, Grid } from 'semantic-ui-react'
 
 class Games extends Component {
   constructor (props) {
@@ -8,33 +8,46 @@ class Games extends Component {
   }
 
   render () {
+
+    let gameCards = [];
+    for (let i = 0; i < 10; i++) {
+      gameCards.push(<Grid.Column>
+            <Card fluid color='violet'>
+              <Card.Content>
+                <Card.Header>
+                  Date
+                </Card.Header>
+                <Card.Meta>
+                  <span className="time">
+                    Time
+                  </span>
+                  <span className="rink">
+                    Rink 
+                  </span>
+                </Card.Meta>
+                <Card.Description>
+                    General game info 
+                </Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <div className='ui two buttons'>
+                  <Button basic color='green' active>Available</Button>
+                  <Button basic color='red'>Unavailable</Button>
+                </div>
+              </Card.Content>
+            </Card>
+            <br/>
+      </Grid.Column>)
+    }
+
     return (
-      <div>
-      <Card>
-        <Card.Content>
-          <Card.Header>
-            June 17th 2017
-          </Card.Header>
-          <Card.Meta>
-            <span className="time">
-              10PM
-            </span>
-            <span className="rink">
-              Rink 1
-            </span>
-          </Card.Meta>
-          <Card.Description>
-              Wearing dark. 
-          </Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-          <div className='ui two buttons'>
-            <Button basic color='green' active>Available</Button>
-            <Button basic color='red'>Unavailable</Button>
-          </div>
-        </Card.Content>
-      </Card>
-      </div>
+
+      <Grid columns={3}>
+      <Grid.Row>
+      {gameCards}    
+      </Grid.Row>
+      </Grid>
+      
     )
   }
 }
