69174487f94b27f96a88eb02f796a0d5b78f79df
grantran <grant.tran@gmail.com>
2017-06-20 18:27:35 +0000

    made banner image work

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -5,7 +5,7 @@ import { Segment, Button, Divider, Image } from 'semantic-ui-react'
 const Landing = () => (
 
   <div>
-    <Image src='client/src/images/organyzr.png' fluid size='large' centered/>
+    <Image src={require('../images/organyzr.png')} size='large' centered/>
     <Segment padded>
       <Button primary fluid>Login</Button>
       <Divider horizontal>Or</Divider>
