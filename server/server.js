2e87e91310d40e057ab7a5f0d2206d05cc3b5257
Spencer Ang <boomerandzapper@gmail.com>
2017-06-21 21:03:00 +0000

    Completed Passport Functionality

--- server/server.js
+++ server/server.js
@@ -19,7 +19,10 @@ const knexConfig  = require("../knexfile");
 const knex        = require("knex")(knexConfig[ENV]);
 const knexLogger  = require('knex-logger');
 
+
+//Routes
 const gamesRoutes = require('./routes/games')
+const loginRoutes = require('./routes/test/login');
 
 const webpack = {
   core: require('webpack'),
@@ -43,7 +46,7 @@ if (ENV === 'development') {
 const add_user_local = require("./functions/add_user_local.js");
 
 // const add_user_local = require("./functions/passport/add_user_local.js");
-// const add_user_facebook = require("./functions/add_user_facebook.js");
+const add_user_facebook = require("./functions/add_user_facebook.js");
 
 
 
@@ -57,7 +60,8 @@ app.use(cookieSession({
 
 
 const passport = require('passport')
- , LocalStrategy = require('passport-local').Strategy;
+ , LocalStrategy = require('passport-local').Strategy
+ , FacebookStrategy = require('passport-facebook').Strategy;
 // FacebookStrategy = require('passport-facebook').Strategy;
 
 passport.use(new LocalStrategy(
@@ -84,26 +88,38 @@ return done(err);
   }
 ));
 
-// passport.use(new FacebookStrategy({
-//     clientID: '891703524347118',
-//     clientSecret: '98717a1f70a79ad745206c6a7e6323f9',
-//     callbackURL: "http://localhost:3000/auth/facebook/callback",
-//     profileFields: ['id', 'email', 'name']
-//   },
-//   function(accessToken, refreshToken, profile, done) {
-//     // User.findOrCreate(..., function(err, user) {
-//     //   if (err) { return done(err); }
-//     //   done(null, user);
-//     // });
-//     console.log('sdfsfd')
-//     console.log(accessToken)
-//     console.log(refreshToken)
-//     console.log(profile)
-//     console.log(done)
-//     knex('users')
-//     done(null, )
-//   }
-// ));
+//CHANGE CALLBACK URL TO WHAT WE USE
+passport.use(new FacebookStrategy({
+    clientID: '891703524347118',
+    clientSecret: '98717a1f70a79ad745206c6a7e6323f9',
+    callbackURL: "http://localhost:8080/auth/facebook/callback",
+    profileFields: ['id', 'email', 'name']
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // User.findOrCreate(..., function(err, user) {
+    //   if (err) { return done(err); }
+    //   done(null, user);
+    // });
+    // add_user_facebook(knex, profile, done)
+
+
+          knex
+          .select()
+          .where({email: profile.emails[0].value})
+          .from("users").first().then(user => {
+      if (!user) {
+        console.log('user not found')
+        return done(null, false, { message: 'Incorrect email.' });
+      }
+
+      return done(null, user);
+          }).catch(function(err) {
+return done(err);
+});
+
+
+  }
+));
 
 
   passport.serializeUser((user, done) => {
@@ -129,24 +145,28 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
-// // Redirect the user to Facebook for authentication.  When complete,
-// // Facebook will redirect the user back to the application at
-// //     /auth/facebook/callback
-// app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email']}));
+// Redirect the user to Facebook for authentication.  When complete,
+// Facebook will redirect the user back to the application at
+//     /auth/facebook/callback
+app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email']}));
 
-// // Facebook will redirect the user to this URL after approval.  Finish the
-// // authentication process by attempting to obtain an access token.  If
-// // access was granted, the user will be logged in.  Otherwise,
-// // authentication has failed.
-// app.get('/auth/facebook/callback',
-//   passport.authenticate('facebook', { successRedirect: '/',
-//                                       failureRedirect: '/login' }));
+// Facebook will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { successRedirect: '/#/',
+                                      failureRedirect: '/#/login' }));
 
 
 // app.use('/games', gamesRoutes(knex));
 
-<<<<<<< HEAD
-=======
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/test/login');
+});
+
+
 app.use(webpack.middleware(compiler, {
   publicPath: webpack.config.output.publicPath,
   noInfo: true,
@@ -156,7 +176,7 @@ app.use(webpack.middleware(compiler, {
 }));
 
 app.use(webpack.hot(compiler));
->>>>>>> 33b0b7487fa73b727fbee58deb77cb4d8531ef2d
+
 
 // Listen to POST requests to /users.
 app.post('/signup', function(req, res) {
@@ -179,40 +199,16 @@ app.post('/login',
     }
 );
 
-app.get('/test/login', function(req, res) {
-    let templateVars = req.session.passport;
-    console.log('templatevars', templateVars)
-    res.render('login', templateVars);
-  });
-app.post('/test/login',
-  passport.authenticate('local',  { successRedirect: '/',
-                                   failureRedirect: '/login',
-                                   failureFlash: false }),
-    function(req, res) {
-      console.log(req)
-      console.log('post to login')
-      res.json({ success: false, message: 'success'})
-    }
-);
 
+app.use('/test/login', loginRoutes(knex, passport));
 app.get('/games/data', function(req, res) {
     console.log('server side');
-    gamesRoutes(knex, res);
+    console.log(req.session.passport.id)
+    gamesRoutes(knex, res, req.session.passport.id);
 })
 
 
-<<<<<<< HEAD
 
-app.use(webpack.middleware(compiler, {
-  publicPath: webpack.config.output.publicPath,
-  noInfo: true,
-  stats: {
-    colors: true
-  }
-}));
-
-
-=======
 // routes to handle react request
 
 // new WebpackDevServer(webpack.core(webpack.config), {
@@ -227,7 +223,7 @@ app.use(webpack.middleware(compiler, {
 //     if (err) {
 //       console.log(err);
 //     }
->>>>>>> 33b0b7487fa73b727fbee58deb77cb4d8531ef2d
+
 
 
 
