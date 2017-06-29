ac78bf048ec39c6bd943e29728cd9b10bdac9d22
Tin Trung Dang <tindang1710@gmail.com>
2017-06-29 12:44:35 -0700

    deactivate automated message for now

--- server/functions/scheduler.js
+++ server/functions/scheduler.js
@@ -1,22 +1,24 @@
-var CronJob = require('cron').CronJob;
-const moment = require('moment');
-const sendNotification = require('./send_notification')
-const schedulerFactory = function() {
-  return {
-    start: function(knex) {
-      console.log('beginning of scheduler')
-      new CronJob('00 27 19 * * *', function() {
-        console.log('Running Send Notifications Worker for ' +
-          moment().format());
-          knex('games_users').select('game_id').returning('game_id')
-          .then((game_id) => {
-            for(let id of game_id) {
-              console.log('show the game id ', id.game_id)
-              sendNotification(knex, id.game_id)
-            }
-          })
-      }, null, true, '');
-    },
-  };
-};
-module.exports = schedulerFactory();
+// var CronJob = require('cron').CronJob;
+// const moment = require('moment');
+// const sendNotification = require('./send_notification')
+// const schedulerFactory = function() {
+//   return {
+//     start: function(knex) {
+//       console.log('beginning of scheduler')
+//       new CronJob('* * * * * *', function() {
+//         console.log('Running Send Notifications Worker for ' +
+//           moment().format());
+//           knex('games_users').join('games', 'games_users.game_id', '=', 'games.id').select('games.id', 'games.date')
+//           .then((games) => {
+//             // console.log(games)
+//             // console.log(games[0].date, games[0].time, games[0].id)
+//             for(let game of games) {
+//               console.log('game day', moment(game.date).calendar())
+//               console.log(moment().add(1, 'days').calendar());
+//             }
+//           })
+//       }, null, true, '');
+//     },
+//   };
+// };
+// module.exports = schedulerFactory();
